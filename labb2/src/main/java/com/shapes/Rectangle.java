b2002fb6f3c9a249ec4b630f7050ba19f1b1e51f
David Lidebrandt <david.lidebrandt@gmail.com>
2023-09-14 18:24:47 +0200

    Inital commit

---
+++ labb2/src/main/java/com/shapes/Rectangle.java
@@ -0,0 +1,17 @@
+package com.shapes;
+
+public class Rectangle extends Shape {
+
+    @Override
+    double getArea() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getArea'");
+    }
+
+    @Override
+    double getPerimeter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPerimeter'");
+    }
+    
+}
---
+++ labb2/src/main/java/com/shapes/Shape.java
@@ -0,0 +1,9 @@
+package com.shapes;
+
+public abstract class Shape {
+
+    abstract double getArea();
+
+    abstract double getPerimeter();
+    
+}
