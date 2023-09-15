adbff994789fc87a1f2a03c6490645a22860cb49
David Lidebrandt <david.lidebrandt@gmail.com>
2023-09-15 22:19:47 +0200

    Add static method for creating shapes

--- labb2/src/main/java/com/labb2/Main.java
+++ labb2/src/main/java/com/labb2/Main.java
@@ -1,7 +1,20 @@
 package com.labb2;
 
+import java.lang.reflect.InvocationTargetException;
+
+import com.shapes.Circle;
+import com.shapes.Rectangle;
+import com.shapes.Shape;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Shape newCircle = Shape.createShape(Circle.class);
+            Shape newRectangle = Shape.createShape(Rectangle.class);
+            System.out.println(newCircle);
+            System.out.println(newRectangle);
+        } catch (Exception e) {
+            System.out.println("An error occurred when trying to create the object");
+        } 
     }
 }
--- labb2/src/main/java/com/shapes/Shape.java
+++ labb2/src/main/java/com/shapes/Shape.java
@@ -1,9 +1,18 @@
 package com.shapes;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public abstract class Shape {
 
     abstract double getArea();
 
     abstract double getPerimeter();
+
+    public static <T extends Shape> Shape createShape(Class<T> shape ) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Constructor<T> shapeConstructor = shape.getConstructor();
+        Shape newShape = shapeConstructor.newInstance();
+        return newShape; 
+    }
     
 }
